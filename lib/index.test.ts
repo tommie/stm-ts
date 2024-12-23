1faa5b180997484fb42b5f2f8c16a3e2a797a16a
Tommie Gannert <tommie@gannert.se>
2024-12-23 10:51:40 +0100

    Adds a test for init functions.

---
+++ lib/index.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "vitest";
+
+import * as cut from "./index";
+
+test("import runs initArray", () => {
+  const got = cut.newRoot([] as number[]);
+
+  expect(() => {
+    cut.inTransaction(() => {
+      got.push(42);
+      throw "abort";
+    });
+  }).toThrow();
+
+  expect(got).not.toContain(42);
+});
